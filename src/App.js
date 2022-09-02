43db42cd0b09620360a32d2ff444e33b16fdc0af
CsilvaD99 <Carlos.silva.clss17@gmail.com>
2022-09-02 08:56:04 -0500

    Deleted double title

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import reducer from "./Reducers/rootReducer";
 function App() {
   return (
     <div className="App">
-      <h1 className="Title">BenchMark</h1>
-
       <h3>
         Welcome to Benckmark, a website designed for anyone wanting to compile
         the best performing computer parts for their future Gaming Pc.
